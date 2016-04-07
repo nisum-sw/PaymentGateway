680e54840a4b4cc307d32c020662ef46fb9781bd
NisumUser <nis1172-mbp@NisumUsers-MacBook-Pro.local>
2016-04-07 09:35:28 -0700

    Adding NisumSafeway Spring Scheduler.

---
+++ NisumSafewayScheduler/src/main/java/com/safeway/quartz/ILatch.java
@@ -0,0 +1,5 @@
+package com.safeway.quartz;
+
+public interface ILatch {
+	void countDown();
+}
---
+++ NisumSafewayScheduler/src/main/java/com/safeway/quartz/TestBean.java
@@ -0,0 +1,7 @@
+package com.safeway.quartz;
+
+public class TestBean {
+	public void doSomething() {
+		System.out.println("muktest/mukarramtest:: Hello MukarramNasri");
+	}
+}
