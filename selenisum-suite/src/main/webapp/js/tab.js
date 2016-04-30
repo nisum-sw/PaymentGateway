668151cc19fe6ad76d66ab5b3c4c1ef9ebf9b743
NisumUser <kamal@kamal.local>
2016-04-29 23:51:02 -0700

    kamal: added new custom.css class and applied to autoSeleniumTab,inputDataTab html files.Modified the loo and feel.Reverted with right files for "will rebase"

--- selenisum-suite/src/main/webapp/js/tab.js
+++ selenisum-suite/src/main/webapp/js/tab.js
@@ -1,29 +1,33 @@
-'use strict';
 var app = angular.module('nAutomationApp', [ 'ngAnimate', 'ui.bootstrap' ]);
 
-app.controller('testSuiteCtrl', ['$scope', '$http',
-   function ($scope, $http) {
+app.controller('testSuiteCtrl', ['$scope', '$http', '$window', '$location',
+                                 function($scope,$http,$window,$location) {
  	var counter=0;
      $scope.testsuiteelemnt = [ {'id':'counter', 'question' : 'TEST-SUITE', 'answer' : '','inline':'true'} ];
-
+     
      $scope.newItem = function($event){
          counter++;
          $scope.testsuiteelemnt.push({'id':'counter', 'question' : 'TEST-SUITE', 'answer' : '','inline':'true'});
          $event.preventDefault();
      }
      
-     $scope.inlinef= function($event,inlinecontrol){
-         var checkbox = $event.target;
-         if(checkbox.checked){
-             //$('#'+ inlinecontrol).css('display','inline');
-         }else{
-             //$('#'+ inlinecontrol).css('display','');
-         }
+     $scope.setSuiteName = function($event) {
+    	 $scope.hideTxtBox = false;
+    	 //$event.preventDefault();
      }
      
-     $scope.showitems = function($event){
-     	//console.log($event);
-        // $('#displayitems').css('visibility','none');
+     $scope.makeSuiteNameStatic = function($event) {
+    	 $scope.hideTxtBox = true;
+    	 //$event.preventDefault();
+     }
+     
+     $scope.addNewTestCase = function($event) {
+    	 $scope.testCases.push({'name':$scope.newTestCaseName, 'selected':false});
+    	 $scope.newTestCaseName = "";
+     }
+     
+     $scope.removeTestCase = function(index,testCase) {
+    	 $scope.testCases.splice(index, 1);
      }
      
 }]);
@@ -60,22 +64,20 @@ app.controller('nAutomationCtrl', function($scope, $window,$location,$http) {
                 });
               })
               .error(function(data, status) {
-                alert(1);
-                alert( "failure message: " + JSON.stringify(data));
           });
 	
 	$scope.reports =[];
-	$http.get(path+"/report?input=s").success(function(data, status) {
+	$http.get(path+"/getReports?input=s").success(function(data, status) {
         console.log(data);
         
-        $scope.reports = data;
-   //     data.forEach(function(suite){
-        	//$scope.reports.push(suite);
-     //   });
+        
+        data.forEach(function(suite){
+        	$scope.reports.push(suite);
+        });
       })
       .error(function(data, status) {
-        alert(1);
-        alert( "failure message: " + JSON.stringify(data));
+       // alert(1);
+        //alert( "failure message: " + JSON.stringify(data));
   });
 
 
@@ -100,6 +102,10 @@ app.controller('nAutomationCtrl', function($scope, $window,$location,$http) {
 	
 	$scope.domains = [ "safeway","Walmart","Gap","Target"];
 	
+	$scope.browsers = [ "Chrome","Firefox","Safari","Explorer"];
+	
+
+
 	$scope.dropDownChnaged = function () {
 	      //alert(url.name);
 	        var valData = $scope.selectedDName + $scope.selectedPageName + "." + $scope.selectedFileExtension;
@@ -119,11 +125,78 @@ app.controller('nAutomationCtrl', function($scope, $window,$location,$http) {
 	                alert( "failure message: " + JSON.stringify(data));
 	              });
 	      };
+
+	      $scope.saveAlert= function () {
+		      alert("foo Mukarram");
+		  };
+		      
+
+	      $scope.jsonData =
+	      {  
+	    		     "April281030":"foo000001"  
+	      };
 	      
+	      $scope.jsonDomainName= "http://192.168.7.85:8080/projects/";
+	      
+	        
+	      $scope.project = {};
+	      
+	      $scope.saveToDatabase = function () {
+	    	  
+	    	alert("tab.js::saveToDatabase");
+	        
+	        console.log(" $scope.project = " + JSON.stringify( $scope.project) );
+	        
+	        
+
+	    	  var config = {
+	    		        'Content-Type': 'application/json'
+	    	    };
+	    	  
+	    	  
+	    	  var pathToMongo =  "http://localhost:8080/projects"
+	    		  
+	              $http.post(pathToMongo,
+	                  $scope.project).success(function(data, status) {
+	                    alert(data);
+	                    $scope.report = data;
+	                    $scope.reportHref = data;
+	                  })
+	                  .error(function(data, status) {
+	                    alert(status);
+	                    alert( "failure message: " + JSON.stringify(data));
+	                  });
+	              
+	              $scope.newWindow = function (reportHref) { 
+	                 $window.open(reportHref);
+	              };
+	          };
+	          
+	      $scope.executeTest = function () {
+		          //alert(url.name);
+		            var path = $location.absUrl().substr(0, $location.absUrl().lastIndexOf("/"))
+		            
+		              //$scope.selectedPage=$scope.selectedPage;
+		              $http.post(path+'/executeTest',
+		                  encodeURIComponent($scope.domainName)).success(function(data, status) {
+		                    //alert("i am" + status);
+		                    alert(data);
+		                    $scope.report = data;
+		                    $scope.reportHref = data;
+		                  })
+		                  .error(function(data, status) {
+		                    alert(status);
+		                    alert( "failure message: " + JSON.stringify(data));
+		                  });
+		              
+		              $scope.newWindow = function (reportHref) { 
+		                 $window.open(reportHref);
+		              };
+		          };
           $scope.newWindow = function (reportHref) { 
               $window.open("Results/"+reportHref);
            };
 
+	
            
-        	 
 });
