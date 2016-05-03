4a247954481d8b409c1cf50da5848c4cdb6f6bcf
udayoff <udayoff@gmail.com>
2016-05-03 12:06:58 -0700

    chng @value

--- selenisum-suite/src/main/java/com/nisum/controller/ProjectController.java
+++ selenisum-suite/src/main/java/com/nisum/controller/ProjectController.java
@@ -1,12 +1,15 @@
 package com.nisum.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,38 +29,35 @@ public class ProjectController {
 		this.projectRepository = projectRepository;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	@RequestMapping(value = "/save", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	public ResponseEntity<Project> create(@RequestBody Project Project) {
 		Project project = projectRepository.save(Project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/findAll", method = RequestMethod.GET)
 	public ResponseEntity<List<Project>> list() {
 		List<Project> lstProject = this.projectRepository.findAll();
 		return new ResponseEntity(lstProject, HttpStatus.ACCEPTED);
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<List<Project>> get(@RequestParam("id") String id) {
 		Project project = this.projectRepository.findOne(id);
 		return new ResponseEntity(project, HttpStatus.ACCEPTED);
 	}
 
-	
-	@RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	@RequestMapping(value = "/{id}", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	public ResponseEntity<Project> update(@RequestParam("id") long id, @RequestBody @Valid Project Project) {
 		Project project = projectRepository.save(Project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}
 
-	@RequestMapping(method = RequestMethod.DELETE)
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json", consumes = "application/json")
 	public ResponseEntity<Boolean> delete(@RequestParam("id") String id) {
 		this.projectRepository.delete(id);
 		return new ResponseEntity<Boolean>(Boolean.TRUE, HttpStatus.OK);
 	}
-	
-	
 
 
 }
